101e21849ffd0b64204c95c9020e873cf570e398
haleb <halebane27@gmail.com>
2022-03-20 21:15:40 +0500

    #1 Первая часть

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField('Название', max_length=128)
+    page = models.IntegerField('Количество страниц', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- core/views.py
+++ core/views.py
@@ -3,6 +3,4 @@ from django.template import Template, Context
 from django.shortcuts import render
 
 def hello(request):
-    t = Template('<h1>Hello, {{ name }}</h1>')
-    c = Context({'name': 'Vasya'})
-    return HttpResponse(t.render(context=c))
+    return render(request, 'core/hello.html', {'name': 'Vasya'})
