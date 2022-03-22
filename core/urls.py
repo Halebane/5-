e7650709cf1f129668617733f1466fcd1e94130e
haleb <halebane27@gmail.com>
2022-03-22 21:38:25 +0500

    #3 Добавила страницу о деталях книги

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 import core.views
 
+app_name = 'core'
+
 urlpatterns = [
-    path('', core.views.index)
+    path('', core.views.index, name='index'),
+    path('books/', core.views.book_list, name='book_list'),
+    path('admin/', core.views.admin, name='admin'),
+    path('books/<int:pk>/', core.views.book_detail, name='book_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 import core.models
 
 def index(request):
     books= core.models.Book.objects.all()
-    return render(request, 'core/index.html', {'books':books})
+    return render(request, 'core/index.html', {'books': books})
+
+def book_list(request):
+    books= core.models.Book.objects.all()
+    return render(request, 'core/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+    return HttpResponseNotFound('Не найдено')
+    book= core.models.Book.objects.get(pk=pk)
+    return render(request, 'core/book_detail.html', {'book': book})
+
+def admin(request):
+    return render(request, 'core/index.html')
