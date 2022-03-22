3d45c7122b0ad139ec1129b7e8f62af604a21f29
haleb <halebane27@gmail.com>
2022-03-22 22:34:18 +0500

    #3 Добавила классы

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ import core.views
 app_name = 'core'
 
 urlpatterns = [
-    path('', core.views.index, name='index'),
-    path('books/', core.views.book_list, name='book_list'),
+    path('', core.views.IndexView.as_view(), name='index'),
+    path('books/', core.views.Books.as_view(), name='book_list'),
     path('admin/', core.views.admin, name='admin'),
-    path('books/<int:pk>/', core.views.book_detail, name='book_detail'),
+    path('books/<int:pk>/', core.views.BookDetail.as_view(), name='book_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,23 +1,62 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+
 import core.models
 
-def index(request):
-    books= core.models.Book.objects.all()
-    return render(request, 'core/index.html', {'books': books})
 
-def book_list(request):
-    name = request.GET.get('name')
-    books = core.models.Book.objects.all()
-    if name:
-        books = books.filter(name__icontains=name)
+class TitleMixin:
+    title = None
 
-    return render(request, 'core/book_list.html', {'books': books})
+    def get_title(self):
+        return self.title
 
-def book_detail(request, pk):
-    return HttpResponseNotFound('Не найдено')
-    book= core.models.Book.objects.get(pk=pk)
-    return render(request, 'core/book_detail.html', {'book': book})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = self.get_title()
+        return context
+
+
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'core/index.html'
+    title = "Главная страница"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['info'] = self.get_info()
+        return context
+
+    def get_info(self):
+        return 'Главная страница'
+
+
+# def index(request):
+# return render(request, 'core/index.html')
+
+class Books(ListView):
+    def get_queryset(self):
+        name = self.request.GET.get('name')
+        queryset = core.models.Book.objects.all()
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+
+# def book_list(request):
+#    name = request.GET.get('name')
+#    books = core.models.Book.objects.all()
+#    if name:
+#        books = books.filter(name__icontains=name)
+#    return render(request, 'core/book_list.html', {'books': books})
+
+class BookDetail(DetailView):
+    queryset = core.models.Book.objects.all()
+
+
+# def book_detail(request, pk):
+#    return HttpResponseNotFound('Не найдено')
+#    book= core.models.Book.objects.get(pk=pk)
+#    return render(request, 'core/book_detail.html', {'book': book})
 
 def admin(request):
     return render(request, 'core/index.html')
