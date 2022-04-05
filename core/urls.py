f7232c6000c432afcd7d97637e3d07942ced6891
haleb <halebane27@gmail.com>
2022-04-05 23:28:49 +0500

    #8 Добавила регистрацию, вход и профиль

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as authViews
+
 import core.views
 
 app_name = 'core'
@@ -10,4 +12,8 @@ urlpatterns = [
     path('books/create/', core.views.BookCreate.as_view(), name='book_create'),
     path('books/<int:pk>/update/', core.views.BookUpdate.as_view(), name='book_update'),
     path('books/<int:pk>/delete/', core.views.BookDelete.as_view(), name='book_delete'),
+    path('login/', core.views.LoginView.as_view(), name="login"),
+    path('profile/', core.views.ProfilePage.as_view(), name="profile"),
+    path('core/profile', core.views.RegisterView.as_view(), name="register"),
+    path('exit/', authViews.LogoutView.as_view(next_page='core:login'), name='exit'),
 ]
