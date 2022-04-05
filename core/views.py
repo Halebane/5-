1e7e759fb6605a5f739b3d5ea5c0d039083e9390
haleb <halebane27@gmail.com>
2022-04-05 23:28:49 +0500

    #8 Добавила регистрацию, вход и профиль

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView, DeleteView
 
@@ -82,3 +85,42 @@ class BookDelete(TitleMixin, DeleteView):
 
     def get_success_url(self):
         return reverse('core:book_list')
+
+
+class LoginView(TemplateView):
+    template_name = "core/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {}
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:profile"))
+            else:
+                context['error'] = "Логин или пароль неправильные"
+        return render(request, self.template_name, context)
+
+
+class ProfilePage(TemplateView):
+    template_name = "core/profile.html"
+
+
+class RegisterView(TemplateView):
+    template_name = "core/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            password2 = request.POST.get('password2')
+
+            if password == password2:
+                User.objects.create_user(username, email, password)
+                return redirect(reverse("core:login"))
+
+        return render(request, self.template_name)
+
